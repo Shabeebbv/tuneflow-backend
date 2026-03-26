81607844cda01675ea2abfcb039b3239999ad859
Mohammed shabeeb <sabishabeeb2@gmail.com>
2026-03-26 22:28:11 +0530

    chore: rename all to common base /api

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -33,13 +33,13 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin-dashboard/', admin.site.urls),
     path("api/", include("products.urls")),
     path("api/", include("cart.urls")),
     path("api/", include("accounts.urls")),
     path("api/", include("orders.urls")),
     path("api/", include("wishlist.urls")),
     path('api/admin/', include('admin_panel.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
 ]
